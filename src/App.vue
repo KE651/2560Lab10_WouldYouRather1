9b7f5d3c898896c83d66dfaa2700d7ec131ba3b2
KE651 <101422949+KE651@users.noreply.github.com>
2022-11-06 21:14:57 -0600

    commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<!-- Had to delete a lot of comments from my code, seemed to interfere -->
+<!-- with the components when placed in certain locations -->
+<!-- also had a difficult to trace error, turned out to be closing bracket of h1 below -->
+<template>
+  <div id="app">
+    <h1>Would you rather...</h1>
+    <would-you-rather
+      v-bind:question="wyrQuestion" 
+      v-bind:answer1="wyrAnswer1"
+      v-bind:answer2="wyrAnswer2"
+      v-on:answer-changed="answerChanged">
+      <!-- when answer-changed event is received from child component, call the method answerChanged-->
+    </would-you-rather>
+        <!-- adds WouldYouRather component -->
+        <!-- v-bind from parent data to child props -->
+    <p>{{ userSelectionMessage }}</p>
+  </div>
+</template>
+
+<!-- v-bind: [prop in wyr.vue] = ["name in App.vue"]-->
+
+<script>
+import WouldYouRather from './components/WouldYouRather.vue'
+export default {
+  name: 'App',  
+  components: {WouldYouRather},
+  data() {
+    return {
+      wyrQuestion: 'Would you rather be able to see things that are very far away, like binoculars, or be able to see things very close up, like a microscope?',
+      wyrAnswer1: 'binocular eyes!',
+      wyrAnswer2: 'microscope eyes!',
+      userSelectionMessage:''
+      //https://conversationstartersworld/would-you-rather-questions/would-you-rather-questions-for-kids #21
+    }
+  }, // end data
+  methods: {
+    answerChanged(choice) {
+     this.userSelectionMessage = `Thanks!  You chose ${choice}.`
+    }
+  } // end methods
+}  // end export
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: darkgreen;
+  margin-top: 60px;
+  border-top:1px fuchsia solid;
+}
+</style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wyr">
+    <h2>Please make your choice!</h2>
+    <h3>{{ question }}</h3>
+
+    <input class="button" type="radio" v-model="choice" 
+      v-bind:value="answer1" v-on:change="choiceMade">
+      <label>{{ answer1 }}</label>
+      
+    <input class="button" type="radio" v-model="choice" 
+      v-bind:value="answer2" v-on:change="choiceMade">
+      <label>{{ answer2 }}</label>
+    <br><br>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {
+    question: String,
+    answer1: String,
+    answer2: String
+    //props are for data given to child by parent, component should not modify props
+  },
+  data() {
+    return {choice:''}
+      //choice will update in devtools when radio buttons are selected
+      //data function is for data that is used internally to the component
+  },
+  methods: {
+    choiceMade() {
+      //emit an event 'answer-changed' with value of selection
+      this.$emit('answer-changed', this.choice)
+      // name of event is 'answer-changed', convention is for dashes in events
+      // $emit(name-of-event, argument1, argument2, ..)
+    }
+  } // end methods
+} // end export
+// props are for data provided by a parent component (App.vue in this case)
+//devtools will show props for this file and data for App.vue
+
+</script>
+
+<style scoped>
+
+.wyr {
+  margin: 30px;
+  border: 4px purple solid;
+  color: black;
+  background-color: azure;
+  font-style:oblique
+}
+</style>
